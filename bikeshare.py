a94cbe068a9a3359fbc9cce6434b27715774ecff
Vikash Kumar <vkse116@gmail.com>
2023-08-09 22:28:57 +0530

    Changed descriptive message in time_stats function

--- bikeshare.py
+++ bikeshare.py
@@ -92,7 +92,7 @@ def load_data(city, month, day):
 
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
+    """Displays Statistics on the most frequent times of travel."""
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
