2623bf60b86346bd5de0da8416ab398b3ff80c00
Vikash Kumar <vkse116@gmail.com>
2023-08-09 22:09:53 +0530

    Changed function description for get_filters()

--- bikeshare.py
+++ bikeshare.py
@@ -10,7 +10,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 
 def get_filters():
     """
-    Asks user to specify a city, month, and day to analyze.
+    Asks user to specify a City, Month, and Day to analyze.
 
     Returns:
         (str) city - name of the city to analyze
